4881e086292969607fa8c6377456a046f41b1c79
Elvis Peter Kitone <elviskitone@gmail.com>
2023-06-05 02:41:46 +0300

    Added the Django Secret Key to .env file

--- world_analytics/settings.py
+++ world_analytics/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+bf*i$@o*8qx66ow$5a6xlmawobltf2x-m5w=m@5z%4lgoa+j='
+SECRET_KEY = env('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
